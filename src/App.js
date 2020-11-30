4312d1ce9396100ee88df9101f806ac5dd6a466b
Rafael Brill <rafabrill@gmail.com>
2020-11-30 17:29:17 -0600

    NavBar&&FBauthentication

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+//import ReactDOM from 'react-dom';
+import { Home } from './Home';
+import { About } from './About';
+import { Contact } from './Contact';
+import { NoMatch } from './NoMatch';
+import { Layout } from './components/Layout';
+import { NavigationBar } from './components/NavigationBar';
+import Facebook from './components/Facebook';
+//import FacebookLogin from 'react-facebook-login';
+//import { Jumbotron } from './components/Jumbotron';
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Router>
+          <NavigationBar />
+          <Facebook />
+          <Layout>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/about" component={About} />
+              <Route path="/contact" component={Contact} />
+              <Route component={NoMatch} />
+            </Switch>
+          </Layout>
+        </Router>
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
---
+++ src/Home.js
@@ -0,0 +1,9 @@
+import React from 'react';
+//import ReactDOM from 'react-dom';
+//import FacebookLogInBtn from 'react-facebook-login'
+//import Facebook from './components/Facebook';
+
+ export const Home = () => (
+   <div>
+   </div>
+ )
