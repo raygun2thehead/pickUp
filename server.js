e87490839ef85ac6b5ebee217f89180b0f637ea8
Christian Ramirez <christian.r0602@gmail.com>
2020-12-12 14:03:51 -0600

    gre

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const session = require('express-session');
 const MongoStore = require('connect-mongo')(session);
 const path = require('path');
+const cors = require('cors');
 
 const mongoose = require("mongoose");
 // const routes = require("./routes");
@@ -11,42 +12,27 @@ const PORT = process.env.PORT || 3001;
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use(cors());
+
+
 // Serve up static assets (usually on heroku)
 const uri = process.env.MONGODB_URI || "mongodb://localhost/pickup02";
-mongoose
-  .connect(uri)
-  .then(
-    () => {
-      /** ready to use. The `mongoose.connect()` promise resolves to undefined. */
-      console.log('Connected to Mongo');
-
-      // Sessions
-      app.use(
-        session({
-          secret: 'fraggle-rock', //pick a random string to make the hash that is generated secure
-          store: new MongoStore({ mongooseConnection: mongoose.connection }),
-          resave: false, //required
-          saveUninitialized: false, //required
-        })
-      );
-
-     
-      
-
-      // Routes
-      // app.use('/api/user', user);
-
-      if (process.env.NODE_ENV === 'production') {
+   if (process.env.NODE_ENV === 'production') {
         app.use(express.static(path.join(__dirname, 'client/build')));
         //
         app.get('*', (req, res) => {
-          res.sendfile(path.join((__dirname = 'client/build/index.html')));
+          console.log('working')
+          res.sendFile(path.join((__dirname = 'client/build/index.html')));
         });
       }
 
       // build mode
       app.get('*', (req, res) => {
+        console.log('build mode')
+        console.log(path.join(__dirname + '/client/public/index.html'))
         res.sendFile(path.join(__dirname + '/client/public/index.html'));
+        
       });
 
       // app.post('/api/user', (req, res) => {
@@ -57,11 +43,4 @@ mongoose
       app.listen(PORT, () => {
         console.log(`App listening on PORT: ${PORT}`);
       });
-    },
-    (err) => {
-      /** handle initial connection error */
-      console.log('error connecting to Mongo: ');
-      console.log(err);
-    }
-  )
-  .catch((err) => console.log({ err }));
+     
