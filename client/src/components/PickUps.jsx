56482912b6ca1dcad4d397c62268876aaacf6641
Steven Schaab <stevenes83@gmail.com>
2020-12-12 21:47:33 -0600

    premerge

--- client/src/components/PickUps.jsx
+++ client/src/components/PickUps.jsx
@@ -2,43 +2,43 @@ import React from 'react';
 
 function Table(props) {
     console.log(props.activities)
-    return (
-        <table className="table">
-            <thead>
-                <tr>
-                    <th scope="col">Activity</th>
-                    <th scope="col">Park</th>
-                    <th scope="col">Date</th>
-                    <th scope="col">Time</th>
-                    <th scope="col">City</th>
-                </tr>
-            </thead>
-            <tbody>
+    // return (
+    //     <table className="table">
+    //         <thead>
+    //             <tr>
+    //                 <th scope="col">Activity</th>
+    //                 <th scope="col">Park</th>
+    //                 <th scope="col">Date</th>
+    //                 <th scope="col">Time</th>
+    //                 <th scope="col">City</th>
+    //             </tr>
+    //         </thead>
+    //         <tbody>
 
-                {activities.length > 0? activities.employees.map(activity=>{
-                    return(
-                        <tr>
-                    {/* <th scope="row"><img src={activity.picture.thumbnail}/></th> */}
-                    <td >{activity.name.first}</td>
-                    <td>{activity.name.last}</td>
-                    <td>{activity.email}</td>
-                    <td>{activity.phone}</td>
+    //             {activities.length > 0? activities.map(activity=>{
+    //                 return(
+    //                     <tr>
+    //                 {/* <th scope="row"><img src={activity.picture.thumbnail}/></th> */}
+    //                 <td >{activity.name.first}</td>
+    //                 <td>{activity.name.last}</td>
+    //                 <td>{activity.email}</td>
+    //                 <td>{activity.phone}</td>
                    
-                </tr> 
-                    )
-                }) :(<tr>
-                    <th scope="row">1</th>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
+    //             </tr> 
+    //                 )
+    //             }) :(<tr>
+    //                 <th scope="row">1</th>
+    //                 <td></td>
+    //                 <td></td>
+    //                 <td></td>
+    //                 <td></td>
                    
-                </tr> )  }
+    //             </tr> )  }
                
                 
-            </tbody>
-        </table>
-    )
+    //         </tbody>
+    //     </table>
+    // )
 
 }
 
