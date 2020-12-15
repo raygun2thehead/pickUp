8b70deed584dcd872477cca015aa54daafa09e27
Christian Ramirez <christian.r0602@gmail.com>
2020-12-14 20:00:56 -0600

    server tweaks

--- server.js
+++ server.js
@@ -18,29 +18,29 @@ app.use(cors());
 
 // Serve up static assets (usually on heroku)
 const uri = process.env.MONGODB_URI || "mongodb://localhost/pickup02";
-   if (process.env.NODE_ENV === 'production') {
-        app.use(express.static(path.join(__dirname, 'client/build')));
-        //
-        app.get('*', (req, res) => {
-          console.log('working')
-          res.sendFile(path.join((__dirname = 'client/build/index.html')));
-        });
-      }
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, './client/build')));
+  //
+  app.get('*', (req, res) => {
+    console.log('working')
+    res.sendFile(path.join((__dirname = './client/build/index.html')));
+  });
+}
 
-      // build mode
-      app.get('*', (req, res) => {
-        console.log('build mode')
-        console.log(path.join(__dirname + '/client/public/index.html'))
-        res.sendFile(path.join(__dirname + '/client/public/index.html'));
-        
-      });
+// build mode
+app.get('*', (req, res) => {
+  console.log('build mode')
+  console.log(path.join(__dirname + './client/public/index.html'))
+  res.sendFile(path.join(__dirname + './client/public/index.html'));
 
-      // app.post('/api/user', (req, res) => {
-      //   console.log('THE ROUTE IS HIT');
-      // });
+});
 
-      // Starting Server
-      app.listen(PORT, () => {
-        console.log(`App listening on PORT: ${PORT}`);
-      });
-     
+// app.post('/api/user', (req, res) => {
+//   console.log('THE ROUTE IS HIT');
+// });
+
+// Starting Server
+app.listen(PORT, () => {
+  console.log(`App listening on PORT: ${PORT}`);
+});
+
